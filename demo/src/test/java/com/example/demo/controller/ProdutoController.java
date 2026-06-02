2a5b5a1ebc925bd2b038f02f87c62a8b2a008ddb
soldado-pratiado <pereira.oliveira.gustavo1610@escola.pr.gov.br>
2026-06-02 08:31:50 -0300

    Update ProdutoController.java

--- demo/src/test/java/com/example/demo/controller/ProdutoController.java
+++ demo/src/test/java/com/example/demo/controller/ProdutoController.java
@@ -3,8 +3,13 @@ package com.example.demo.controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.example.demo.repository.ProdutoRepository;
+
     @RestController
     @RequestMapping ("/produto")
 public class ProdutoController {
-      
+      private final ProdutoRepository produtoRepository;
+      public ProdutoController(ProdutoRepository produtoRepository){
+        this.produtoRepository = produtoRepository;
+      }
 }
