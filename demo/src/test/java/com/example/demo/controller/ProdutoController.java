37df82f26c701dbb6b7879b1357d855822497a60
soldado-pratiado <pereira.oliveira.gustavo1610@escola.pr.gov.br>
2026-06-02 08:25:23 -0300

    Update ProdutoController.java

--- demo/src/test/java/com/example/demo/controller/ProdutoController.java
+++ demo/src/test/java/com/example/demo/controller/ProdutoController.java
@@ -1,5 +1,10 @@
 package com.example.demo.controller;
 
-public class ProdutoController {
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+    @RestController
+    @RequestMapping ("/produto")
+public class ProdutoController {
+      
 }
