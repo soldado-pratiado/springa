328aec49e4ab8e74411e0f0e49307a2fdf230bb6
soldado-pratiado <pereira.oliveira.gustavo1610@escola.pr.gov.br>
2026-06-11 10:17:23 -0300

    Update ProdutoController.java

--- demo/src/test/java/com/example/demo/controller/ProdutoController.java
+++ demo/src/test/java/com/example/demo/controller/ProdutoController.java
@@ -1,9 +1,12 @@
 package com.example.demo.controller;
 
+import java.math.BigDecimal;
+
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.example.demo.domain.Produto;
 import com.example.demo.repository.ProdutoRepository;
 
     @RestController
@@ -15,7 +18,15 @@ public class ProdutoController {
       }
 
       @PostMapping("/cadastrar")
-      public static void cadastrar(){
+      public void cadastrar(){
+
+        Produto p1 = new Produto();
+        p1.setNome("Leite");
+        p1.setQuantidade(50);
+        p1.setSetor("Alimentos");
+        p1.setPreco(new BigDecimal(5.99));
+
+        produtoRepository.save(p1);
         
       }
 }
